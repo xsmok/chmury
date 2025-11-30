c9dd92f9bc1ccaa34a6f29508122739d3e5d1f64
xsmok <adrian.klimpel@gmail.com>
2025-11-30 19:03:24 +0100

    Enhance database connection by adding port, socket, and SSL options

--- db.php
+++ db.php
@@ -8,7 +8,10 @@ $db = new mysqli(
     $config['db_host'],
     $config['db_user'],
     $config['db_pass'],
-    $config['db_name']
+    $config['db_name'],
+    3306,
+    null,
+    MYSQLI_CLIENT_SSL
 );
 
 $db->set_charset('utf8mb4');
