76f62c6e14c37b56a7f494f6a0d777cecdc6b85e
xsmok <adrian.klimpel@gmail.com>
2025-11-30 18:37:50 +0100

    Add PHP-FPM startup command to Nginx configuration script

--- startup.sh
+++ startup.sh
@@ -3,5 +3,8 @@
 # Kopiowanie konfiguracji Nginx
 cp /home/site/wwwroot/default /etc/nginx/sites-available/default
 
+# Uruchomienie PHP-FPM
+service php8.4-fpm start || service php8.3-fpm start || service php8.2-fpm start || service php-fpm start
+
 # Restart Nginx
 service nginx reload
